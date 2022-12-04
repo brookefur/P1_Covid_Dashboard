f81a396034dbc320cd374906f9785d7984a360ee
brookefur <56653966+brookefur@users.noreply.github.com>
2022-12-04 22:48:32 +0000

    organized files by part 1 and part 2, made a new JSON so we hit the 2 file requirement

--- Project Part 1/test_read.py
+++ Project Part 1/test_read.py
@@ -13,11 +13,11 @@ import seaborn as sns
 
 
 
-out=ScrapeWebsite.scrape_country('france','worldometer')
-print(out)  
+#out=ScrapeWebsite.scrape_country('france','worldometer')
+#print(out)  
   
 
-sns.lineplot(data=out,x='Index',y='Daily Deaths')
+#sns.lineplot(data=out,x='Index',y='Daily Deaths')
 
 #data2=ScrapeWebsite.OurWorldData_json() # this works and downloads a json of all current covid data
 
@@ -27,8 +27,8 @@ sns.lineplot(data=out,x='Index',y='Daily Deaths')
 
 ## testing json list writing
 
-'''
-short_list=['us','france','greece','germany']
+
+short_list=['us','france','greece','germany','spain']
 ScrapeWebsite.json_frame_writer(short_list,'worldometer')
 
 
@@ -37,6 +37,6 @@ with open('CovidData.json') as infile:
     for name in data.keys():
         US_data=pd.DataFrame(data[name])
         print("Total Deaths in "+ name +': ' + str(US_data['Total Deaths'][-1]) + ' at the current date of: '+ str(US_data['Dates'][-1]))
- '''   
+    
 
 
