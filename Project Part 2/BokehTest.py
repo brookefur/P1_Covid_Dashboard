c052793d9929378e82200f4e7bf881b455945fe5
brookefur <56653966+brookefur@users.noreply.github.com>
2022-12-08 23:31:30 +0000

    added bar graph code to DataDisplay.py I need to make the html page cute, and then try to figure out linking widgets and the plots. Current wants are - to change the day that the bar chart is taken from - change line data from daily deaths to total deaths/ deaths per 100k

--- Project Part 2/BokehTest.py
+++ Project Part 2/BokehTest.py
@@ -3,7 +3,9 @@ import pandas as pd
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import file_html
-
+from bokeh.models import MultiChoice, CustomJS
+from bokeh.palettes import viridis
+from bokeh.layouts import column
 """
 This is a test file for running bokeh and creating html files. I will be creating the official file later,
 this is just a little file for all the important kewords/commands I need to remember.
@@ -13,26 +15,42 @@ Also: I'm assuming our profs open the html file, and I need to make the html fil
 @author: Brooke Boone
 """
 
-filef = open('CovidData.json')
+#open file, get data
+filef = open('CovidData.2022-12-03.json')
 covidData = json.load(filef)
 filef.close()
 
-dates = pd.to_datetime(list(covidData['us']['Dates'].values()))
-deaths = list(covidData['us']['Daily Deaths'].values())
+numCountries = len(list(covidData.keys()))
+nameCountries = []
+dailyDeaths = []
+totalDeaths = []
+key1 = list(covidData.keys())[0]
+dates = pd.to_datetime(list(covidData[key1]['Dates'].values()))
+#for now, assuming all dates for all countries are the same
 
-#When setting plot for dates, make sure to adjust x-axis to start at 2020-02-15
-#LINE GRAPH
-plot = figure(x_axis_type="datetime")
-plot.line(dates,deaths)
+#this takes all data in the dictionary into lists. We need lists for plotting
+for i in covidData.keys():
+    nameCountries.append(i)
+    dailyDeaths.append(list(covidData[i]['Daily Deaths'].values()))
+    totalDeaths.append(list(covidData[i]['Total Deaths'].values()))
+
+lineplot = figure(x_axis_type="datetime")
+colorNames = viridis(len(nameCountries))
+for i in range(0,len(nameCountries)):
+    Cname = nameCountries[i]
+    lineplot.line(dates,dailyDeaths[i],legend_label = Cname,line_color = colorNames[i])
+lineplot.legend.click_policy="hide"
 
 
-
-
-html = file_html(plot, CDN, "COVID Dashboard")
-
-f = open('test_html.html', 'w')
+p = column(lineplot)
+html = file_html(p, CDN, "COVID Dashboard")
+f = open('TEST.html', 'w')
 f.write(html)
 f.close()
+
+
+
+
 
 # $.getJSON('mydata.json', function(data) {});
 # Put in html, use to access data
--- Project Part 2/DataDisplay.py
+++ Project Part 2/DataDisplay.py
@@ -4,6 +4,7 @@ import matplotlib.colors as mcolors
 from bokeh.plotting import figure
 from bokeh.resources import CDN
 from bokeh.embed import file_html
+from bokeh.layouts import row
 
 """
 This file takes the json file from the datascraping, and uses Bokeh to make an interactive html site
@@ -12,33 +13,43 @@ This file takes the json file from the datascraping, and uses Bokeh to make an i
 """
 
 #open file, get data
-filef = open('CovidData.json')
+filef = open('CovidData.2022-12-03.json')
 covidData = json.load(filef)
 filef.close()
 
 numCountries = len(list(covidData.keys()))
 nameCountries = []
 dailyDeaths = []
+totalDeaths = []
 key1 = list(covidData.keys())[0]
 dates = pd.to_datetime(list(covidData[key1]['Dates'].values()))
-#for now, assuming all dates for all countries are the same
+#all dates for all countries are the same
 
 #this takes all data in the dictionary into lists. We need lists for plotting
 for i in covidData.keys():
     nameCountries.append(i)
     dailyDeaths.append(list(covidData[i]['Daily Deaths'].values()))
+    totalDeaths.append(list(covidData[i]['Total Deaths'].values()))
 
 
-plot = figure(x_axis_type="datetime")
+lineplot = figure(x_axis_type="datetime")
 colorNames = list(mcolors.CSS4_COLORS.keys())
 for i in range(0,len(nameCountries)):
     Cname = nameCountries[i]
-    plot.line(dates,dailyDeaths[i],legend_label = Cname,line_color = colorNames[i])
+    lineplot.line(dates,dailyDeaths[i],legend_label = Cname,line_color = colorNames[i+20])
+lineplot.legend.click_policy="hide"
 
-#plot.line(dates,dailyDeaths[0], legend_label = "country1")
-#plot.line(dates,dailyDeaths[1], legend_label = "country2")
 
-html = file_html(plot, CDN, "COVID Dashboard")
+#this is for the bar graph, it takes the total deaths
+topDeaths = []
+for i in totalDeaths:
+    topDeaths.append(i[-1])
+
+barplot = figure(x_range=nameCountries)
+barplot.vbar(x=nameCountries, top=topDeaths, width=0.9)
+
+p = row(lineplot, barplot)
+html = file_html(p, CDN, "COVID Dashboard")
 f = open('CovidDashboard.html', 'w')
 f.write(html)
 f.close()
