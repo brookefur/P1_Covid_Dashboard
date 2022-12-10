6c23856d7b5caee4c913879a145c527b6988f32d
brookefur <56653966+brookefur@users.noreply.github.com>
2022-12-10 19:58:55 +0000

    Made datadisplay into a function

--- Project Part 2/DataDisplay.py
+++ Project Part 2/DataDisplay.py
@@ -16,114 +16,115 @@ This file takes the json file from the datascraping, and uses Bokeh to make an i
 
 @author: Brooke Boone
 """
+def dataDisplay(jsonName):
+    #assuming jsonName is correct
+    #open file, get data
+    filef = open(jsonName)
+    covidData = json.load(filef)
+    filef.close()
 
-#open file, get data
-filef = open('CovidData.2022-12-09.full.json')
-covidData = json.load(filef)
-filef.close()
-
-#Get all of the data out of the dictionaries, and into lists I can use
-numCountries = len(list(covidData.keys()))
-nameCountries = []
-dailyDeaths = []
-totalDeaths = []
-dailyDper100 = []
-totalDper100 = []
-key1 = list(covidData.keys())[0]
-dates = pd.to_datetime(list(covidData[key1]['Dates'].values()))
-#all dates for all countries are the same
-#this adds data in order of the countries
-for i in covidData.keys():
-    nameCountries.append(i)
-    dailyDeaths.append(list(covidData[i]['Daily Deaths'].values()))
-    totalDeaths.append(list(covidData[i]['Total Deaths'].values()))
-    dailyDper100.append(list(covidData[i]['Daily Deaths per 100k'].values()))
-    totalDper100.append(list(covidData[i]['Total Deaths per 100k'].values()))
-
-
-#plotting historical data as a line graph
-lineplot = figure(x_axis_type="datetime", title="Daily Deaths since 2022-02-15",height=600,width=600)
-colorNames = viridis(len(nameCountries))
-for i in range(0,len(nameCountries)):
-    Cname = nameCountries[i]
-    lineplot.line(dates,dailyDeaths[i],legend_label = Cname,line_color = colorNames[i])
-lineplot.legend.click_policy="hide"
-lineplot.xaxis.axis_label = "Select a country in the legend to hide it."
+    #Get all of the data out of the dictionaries, and into lists I can use
+    numCountries = len(list(covidData.keys()))
+    nameCountries = []
+    dailyDeaths = []
+    totalDeaths = []
+    dailyDper100 = []
+    totalDper100 = []
+    key1 = list(covidData.keys())[0]
+    dates = pd.to_datetime(list(covidData[key1]['Dates'].values()))
+    #all dates for all countries are the same
+    #this adds data in order of the countries
+    for i in covidData.keys():
+        nameCountries.append(i)
+        dailyDeaths.append(list(covidData[i]['Daily Deaths'].values()))
+        totalDeaths.append(list(covidData[i]['Total Deaths'].values()))
+        dailyDper100.append(list(covidData[i]['Daily Deaths per 100k'].values()))
+        totalDper100.append(list(covidData[i]['Total Deaths per 100k'].values()))
 
 
-#this is for the bar graph, it takes the total deaths
-topDeaths = []
-for i in totalDeaths:
-    topDeaths.append(i[-1])
-titletext = "Total Covid Deaths on " + dates[-1].strftime("%m/%d/%Y")
-barplot = figure(x_range=nameCountries, title=titletext,height = 600,width=600)
-barplot.vbar(x=nameCountries, top=topDeaths, width=0.9)
+    #plotting historical data as a line graph
+    lineplot = figure(x_axis_type="datetime", title="Daily Deaths since 2022-02-15",height=600,width=600)
+    colorNames = viridis(numCountries)
+    for i in range(0,numCountries):
+        Cname = nameCountries[i]
+        lineplot.line(dates,dailyDeaths[i],legend_label = Cname,line_color = colorNames[i])
+    lineplot.legend.click_policy="hide"
+    lineplot.xaxis.axis_label = "Select a country in the legend to hide it."
 
 
-# only using 2 countries, displaying all of their data
-#Make title be htmal or something, it gets cut off
-titletext="Data for " + nameCountries[0].upper() + " and "+nameCountries[1].upper() + " on " + dates[-1].strftime("%m/%d/%Y")
-
-x1 = ["Daily Deaths"]
-x2 = [nameCountries[0],nameCountries[1]]
-xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
-yplot = (dailyDeaths[0][-1],dailyDeaths[1][-1])
-smallplot1 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="")
-smallplot1.vbar(x=xplot, top=yplot, width=0.2, )
-
-x1 = ["Total Deaths"]
-x2 = [nameCountries[0],nameCountries[1]]
-xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
-yplot = (totalDeaths[0][-1],totalDeaths[1][-1])
-smallplot2 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="", title=titletext)
-smallplot2.vbar(x=xplot, top=yplot, width=0.2 )
-
-x1 = ["Daily Deaths per 100k"]
-x2 = [nameCountries[0],nameCountries[1]]
-xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
-yplot = (dailyDper100[0][-1],dailyDper100[1][-1])
-smallplot3 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="")
-smallplot3.vbar(x=xplot, top=yplot, width=0.2 )
-
-x1 = ["Total Deaths per 100k"]
-x2 = [nameCountries[0],nameCountries[1]]
-xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
-yplot = (totalDper100[0][-1],totalDper100[1][-1])
-smallplot4 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="")
-smallplot4.vbar(x=xplot, top=yplot, width=0.2)
-
-smallplots1 = row(smallplot1,smallplot2)
-smallplots2 = row(smallplot3,smallplot4)
-smallplot = column(smallplots1,smallplots2)
+    #this is for the bar graph, it takes the total deaths
+    topDeaths = []
+    for i in totalDeaths:
+        topDeaths.append(i[-1])
+    titletext = "Total Covid Deaths on " + dates[-1].strftime("%m/%d/%Y")
+    barplot = figure(x_range=nameCountries, title=titletext,height = 600,width=600)
+    barplot.vbar(x=nameCountries, top=topDeaths, width=0.9)
 
 
-#interactive plot, uses tabs
-tabs = []
-for i in range(0,numCountries):
-    plot = figure(x_axis_type="datetime",width=600, height=600,title="Daily Deaths per 100k")
-    plot.line(dates,dailyDper100[i])
-    tab = TabPanel(child=plot,title=nameCountries[i])
-    tabs.append(tab)
-tabplot=Tabs(tabs=tabs)
+    # only using 2 countries, displaying all of their data
+    #Make title be htmal or something, it gets cut off
+    titletext="Data for " + nameCountries[0].upper() + " and "+nameCountries[1].upper() + " on " + dates[-1].strftime("%m/%d/%Y")
 
-htmlTitle = """
- <!DOCTYPE html>
-<html>
-<body>
-<h1>Covid Dashboard 2022</h1>
-<p>By James Rosenberg and Brooke Boone</p>
+    x1 = ["Daily Deaths"]
+    x2 = [nameCountries[0],nameCountries[1]]
+    xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
+    yplot = (dailyDeaths[0][-1],dailyDeaths[1][-1])
+    smallplot1 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="")
+    smallplot1.vbar(x=xplot, top=yplot, width=0.2, )
 
-</body>
-</html> 
-"""
+    x1 = ["Total Deaths"]
+    x2 = [nameCountries[0],nameCountries[1]]
+    xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
+    yplot = (totalDeaths[0][-1],totalDeaths[1][-1])
+    smallplot2 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="", title=titletext)
+    smallplot2.vbar(x=xplot, top=yplot, width=0.2 )
+
+    x1 = ["Daily Deaths per 100k"]
+    x2 = [nameCountries[0],nameCountries[1]]
+    xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
+    yplot = (dailyDper100[0][-1],dailyDper100[1][-1])
+    smallplot3 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="")
+    smallplot3.vbar(x=xplot, top=yplot, width=0.2 )
+
+    x1 = ["Total Deaths per 100k"]
+    x2 = [nameCountries[0],nameCountries[1]]
+    xplot = [(x1s,x2s) for x1s in x1 for x2s in x2]
+    yplot = (totalDper100[0][-1],totalDper100[1][-1])
+    smallplot4 = figure(x_range=FactorRange(*xplot), height=300,width=300,toolbar_location=None, tools="")
+    smallplot4.vbar(x=xplot, top=yplot, width=0.2)
+
+    smallplots1 = row(smallplot1,smallplot2)
+    smallplots2 = row(smallplot3,smallplot4)
+    smallplot = column(smallplots1,smallplots2)
+
+
+    #interactive plot, uses tabs
+    tabs = []
+    for i in range(0,numCountries):
+        plot = figure(x_axis_type="datetime",width=600, height=600,title="Daily Deaths per 100k")
+        plot.line(dates,dailyDper100[i])
+        tab = TabPanel(child=plot,title=nameCountries[i])
+        tabs.append(tab)
+    tabplot=Tabs(tabs=tabs)
+
+    htmlTitle = """
+        <!DOCTYPE html>
+        <html>
+        <body>
+        <h1>Covid Dashboard 2022</h1>
+        <p>By James Rosenberg and Brooke Boone</p>
+        </body>
+        </html> 
+    """
 
 
 
-#put all the plots into rows and columns
-row1 = row( barplot,smallplot) #historical data, bar with all countries and 1 piece of data
-row2 = row(lineplot,tabplot) # data for first 2 countries, interactive plot
-dashboard = column(row1,row2)
-html = htmlTitle + file_html(dashboard, CDN, "COVID Dashboard")
-f = open('CovidDashboard.html', 'w')
-f.write(html)
-f.close()
+    #put all the plots into rows and columns
+    row1 = row( barplot,smallplot) #historical data, bar with all countries and 1 piece of data
+    row2 = row(lineplot,tabplot) # data for first 2 countries, interactive plot
+    dashboard = column(row1,row2)
+    html = htmlTitle + file_html(dashboard, CDN, "COVID Dashboard")
+    f = open('CovidDashboard.html', 'w')
+    f.write(html)
+    f.close()
+
