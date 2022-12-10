e993b766f4fd751f3c2c61410f9e642b1915fa1f
brookefur <56653966+brookefur@users.noreply.github.com>
2022-12-10 20:29:48 +0000

    Updated Readme with bokeh version, made sure the project ran properly

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # P1_Covid_Dashboard
 This is a project for MECH 5250. 
+
+Bokeh version 3.0.2
+(This is important for certain imports like TabPanels)
--- Run_project.py
+++ Run_project.py
@@ -17,7 +17,7 @@ file_name = f'CovidData.{date_str}.json'
 # calls for part 1:
 Country_list=['us','india','france','germany','brazil','japan','italy','russia','turkey','spain','taiwan','iran'] # The list of countries selected for dashbaord
 ScrapeWebsite.json_frame_writer(Country_list,'worldometer',file_name) # writes a nested dict json with all the data for country list
-# Call for generating Dashbaord:
+# Call for generating Dashboard:
 DataDisplay.dataDisplay(file_name)# runs the Dashboard html creation with the current covid data written from above.
 
 
